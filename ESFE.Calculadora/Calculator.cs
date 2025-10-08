94225c2407732aa1e2486a4a520b9a1fa0eef691
VanessaGiron11 <hubgit079@gmail.com>
2025-10-07 20:24:42 -0600

    Pruebas de calculatorTests aprobados

--- ESFE.Calculadora/Calculator.cs
+++ ESFE.Calculadora/Calculator.cs
@@ -19,7 +19,7 @@
 
         public double Divide(double num1, double num2)
         {
-            if (num1 > num2)
+            if (num2 == 0)
             {
                 throw new ArgumentException("Division by zero not allowed.");
             }
